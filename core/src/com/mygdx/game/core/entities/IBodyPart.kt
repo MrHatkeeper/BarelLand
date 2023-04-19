d6980115c1b5b3e8d1ba36a65ca0d9c03e440823
MrHatkeeper <karel.krch@educanet.cz>
2023-04-19 11:28:11 +0200

    BodyParts bro

--- core/src/com/mygdx/game/core/entities/IBodyPart.kt
+++ core/src/com/mygdx/game/core/entities/IBodyPart.kt
@@ -4,6 +4,7 @@ import com.mygdx.game.core.items.armors.IArmor
 
 interface IBodyPart {
     var bodyPartTag: BodyPartTag
+    var isMainPart: Boolean
     var maxHealth: Int
     var actHealth: Int
     var armor: IArmor?
--- core/src/com/mygdx/game/core/entities/IEntity.kt
+++ core/src/com/mygdx/game/core/entities/IEntity.kt
@@ -1,12 +1,13 @@
 package com.mygdx.game.core.entities
 
-import com.mygdx.game.core.items.armors.IArmor
 import com.mygdx.game.core.items.weapons.IWeapon
 
 interface IEntity {
     var weapon: IWeapon?
-    var bodyParts: Map<String,IBodyPart>
+    var bodyParts: Map<String, IBodyPart>
+    fun attack(entity: IEntity)
 
-    fun attack(entity:IEntity)
+    fun destroyedBodyParts(): Int = bodyParts.values.count { it.actHealth <= 0 }
+    fun destroyedMainBodyParts(): Int = bodyParts.values.count{it.actHealth <= 0 && it.isMainPart}
 
 }
